49d9f9a96691ee7b54601cb61597c32973cf9fac
DarkHavoc <darksidevoid@duck.com>
2026-05-09 10:50:58 +0530

    day 1

---
+++ cpppractise/day1IO.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //1 // int n ;
+    //  cin>>n;
+    //  if(n%2==0)
+    //  cout<<("even");
+    //  else
+    //  cout<<("odd");
+     
+    //2nd// int n ;
+    // cin>>n;
+    // if(n>0) cout<<("positive");
+    // else if(n<0)  cout<<("negative");
+    // else  cout<<("zero");
+     
+   //3rd  // int n ;
+    // cin>>n;
+    // if(n%15==0)
+    // cout<<("yes");
+    // else cout<<("no");
+    //4rth // int n,x;
+    // cin>>n>>x;
+    // if(n>x)
+    // cout<<n;
+    // else cout<<x;
+//5th//  int n;
+//      cin>>n ;
+// if(n>=40)
+// cout<<("pass");
+// else cout<<("fail");
+
+//6th//  int n;
+//      cin>>n ;
+//      if(n>18)
+//      cout<<("vote");
+//      else cout<<("can not vote");
+//    
+
+//7th// int n,x;
+    //  cin>>n ;
+    //  x=n%10;
+    //  if (x>0)
+    //  cout<<x;
+    //  else if (n<0)
+    //  cout<<-x;
+    //  else cout<<0;
+    //8rth//  int n;
+    //  cin>>n ;
+    //  if(n==0 or n==24)
+    //  cout<<("midnight");
+    //  if(n==12)
+    //  cout<<("NOON");
+    //  if(n>0 & n<12 )
+    //  cout<<("DAY");
+    //  if(n>12 & n<24)
+    //  cout<<("NIGHT");
+//9th//  int n;
+//      cin>>n ;
+//      if(n>500)
+//      cout<<("INSUFFICIENT BALANCE");
+//      if(n<500 & n>0)
+//      cout<<("WITHDRAWAL SUCCESSFUL");
+//      if(n<0)
+//      cout<<("INVALID AMOUNT");
+     
+//10th//  int n;
+//      cin>>n ;
+//      if(n%4==0)
+//      cout<<("LEAP YEAR");
+//      else
+//      cout<<("NOT A LEAP YEAR");
+return 0;
+} 
